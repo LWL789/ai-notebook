4b4331347df2eb9d026e0645bdb7aef49b2d5aed
LWL789 <lwl20252233002@163.com>
2026-08-24 22:17:15 +0800

    Update models.py

--- models.py
+++ models.py
@@ -30,7 +30,7 @@ class WrongNote(Base):
 User.notes = relationship("WrongNote", order_by=WrongNote.id, back_populates="user")
 import os
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 def init_db():
